cf574ec8c1452e16b3e8278e65bdd488e32c525f
Victor Parma <victorfp@ciandt.com>
2024-09-24 15:42:08 -0300

    removing manually

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 print("first commit for test")
 
 
-print("Add url with secret for example: http://abc:***REMOVED***@repo.com")
+print("Add url with secret for example: http://abc:test@repo.com")
 
 print("another commit for test")
 
